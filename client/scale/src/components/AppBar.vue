5bce15c72bdb0d2466c862630973e07f17963fe8
repliy <repliy.zang.1985@gmail.com>
2020-05-21 16:49:01 +0800

    [+] add mt logo

--- client/scale/src/components/AppBar.vue
+++ client/scale/src/components/AppBar.vue
@@ -1,24 +1,27 @@
 <template>
-      <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="../assets/logo.png"
+        transition="scale-transition"
+        width="150"
+      />
+    </div>
+    <v-spacer></v-spacer>
+    <div class="d-flex align-center">
       <v-btn
-        target="_blank"
         text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-      <v-spacer></v-spacer>
-      <div class="d-flex align-center">
-        <v-btn
-          text
-          @click="clickRightBtn"
-        >{{rightBtnTitle}}</v-btn>
-      </div>
-    </v-app-bar>
+        @click="clickRightBtn"
+      >{{rightBtnTitle}}</v-btn>
+    </div>
+  </v-app-bar>
 </template>
 
 <script>
@@ -32,7 +35,7 @@ export default {
       default: ''
     }
   },
-  mounted() {},
+  mounted() { },
   methods: {
     clickRightBtn() {
       this.$emit('clickRightBtn')
